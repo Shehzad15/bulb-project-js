46aa40643d657a5342c2660b485fde5c8195123e
Shehzad15 <shehzadbhata1@gmail.com>
2022-11-21 17:18:58 +0530

    complete

---
+++ index.js
@@ -0,0 +1,9 @@
+function bulb_on (){
+    document.getElementById('bulb').src = "img/light-bulb-on-off-png-11553940319kdxsp3rf0i-removebg-preview.png";
+
+}
+function bulb_off (){
+    document.getElementById('bulb').src = "img/light-bulb-on-off-png-115539402943y50vxr5yi-removebg-preview.png";
+
+}
+
